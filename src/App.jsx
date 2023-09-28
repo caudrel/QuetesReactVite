1b25915bea7b09b8f54000467867d8d7345d4196
caudrel <lozachaurelie@gmail.com>
2023-09-28 16:22:01 +0200

    working with instructions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const pokemon = pokemonList[0];
+  console.log(pokemon);
+
   return (
     <>
       <div>
-        <PokemonCard />
+        <PokemonCard pokemon={pokemon} />
       </div>
     </>
   );
